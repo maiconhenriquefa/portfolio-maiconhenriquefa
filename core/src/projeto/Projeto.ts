7dfc3c011ee6b8a9dc36ca4a2002cd84d4586198
Maicon Henrique <maiconhenriqufa@gmail.com>
2025-03-19 00:21:06 -0300

    feat: adiciona modelos de Projeto, Tecnologia e enums de Nivel e Tipo

---
+++ core/src/projeto/Projeto.ts
@@ -0,0 +1,14 @@
+import Tecnologia from "../tecnologia/Tecnologia";
+import { Nivel } from "./Nivel";
+import { Tipo } from "./Tipo";
+
+export default interface Projeto {
+  id: number;
+  nome: string;
+  descricao: string;
+  imagens: string[];
+  nivel: Nivel;
+  tipo: Tipo;
+  destaque: boolean;
+  tecnologias: Tecnologia[];
+}
---
+++ core/src/projeto/Tipo.ts
@@ -0,0 +1,5 @@
+export enum Tipo {
+  JOGO = "jogo",
+  MOBILE = "mobile",
+  WEB = "web",
+}
